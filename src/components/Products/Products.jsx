af880f5bfd8de8367a23fc26dd1a92d9dfdaec08
LucianoEpifani <85083543+lucianoEpifani@users.noreply.github.com>
2023-08-22 18:50:00 +0200

    update

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,18 +4,33 @@ import shirt1 from "../../assets/products/p1.jpg";
 import shirt2 from "../../assets/products/p2.jpg";
 import shirt3 from "../../assets/products/p3.jpg";
 import shirt4 from "../../assets/products/p4.jpg";
+import shirt5 from "../../assets/products/f1.jpg";
+import shirt6 from "../../assets/products/f2.jpg";
+import shirt7 from "../../assets/products/f3.jpg";
+import shirt8 from "../../assets/products/f4.jpg";
+import shirt9 from "../../assets/products/f7.jpg";
+import shirt10 from "../../assets/products/f8.jpg";
+import shirt11 from "../../assets/products/n6.jpg";
 import "./Products.css";
 import Nav from "../Nav/Nav";
 
 const products = [
   { id: 1, image: shirt1, title:"Camisa",price: "49.99 EUR", category: 'shirt',},
-  { id: 2, image: shirt2, title:"Camisa",price: "29.99 EUR" , category: 'pants'},
-  { id: 3, image: shirt3, title:"Camisa",price: "49.99 EUR", category: 'top'},
-  { id: 4, image: shirt4, title:"Camisa",price: "19.99 EUR", category: 'boides'},
-  { id: 5, image: shirt1, title:"Camisa",price: "29.99 EUR", category: 'hat'},
-  { id: 6, image: shirt2, title:"Camisa",price: "39.99 EUR",category: 'bikini'},
-  { id: 7, image: shirt3, title:"Camisa",price: "45.99 EUR", category: 'dress'},
-  { id: 8, image: shirt4, title:"Camisa",price: "22.99 EUR", category: 'vest'},
+{ id: 2, image: shirt2, title:"Camisa",price: "29.99 EUR" , category: 'pants'},
+{ id: 3, image: shirt3, title:"Camisa",price: "49.99 EUR", category: 'top'},
+{ id: 4, image: shirt4, title:"Camisa",price: "19.99 EUR", category: 'boides'},
+{ id: 5, image: shirt5, title:"Camisa",price: "29.99 EUR", category: 'hat'},
+{ id: 6, image: shirt6, title:"Camisa",price: "39.99 EUR",category: 'bikini'},
+{ id: 7, image: shirt7, title:"Camisa",price: "45.99 EUR", category: 'dress'},
+{ id: 8, image: shirt8, title:"Camisa",price: "22.99 EUR", category: 'vest'},
+{ id: 9, image: shirt10, title:"Camisa",price: "49.99 EUR", category: 'shirt',},
+{ id: 10, image: shirt11, title:"Camisa",price: "29.99 EUR" , category: 'pants'},
+{ id: 11, image: shirt9, title:"Camisa",price: "49.99 EUR", category: 'top'},
+{ id: 12, image: shirt4, title:"Camisa",price: "19.99 EUR", category: 'boides'},
+{ id: 13, image: shirt1, title:"Camisa",price: "29.99 EUR", category: 'hat'},
+{ id: 14, image: shirt2, title:"Camisa",price: "39.99 EUR",category: 'bikini'},
+{ id: 15, image: shirt3, title:"Camisa",price: "45.99 EUR", category: 'dress'},
+{ id: 16, image: shirt4, title:"Camisa",price: "22.99 EUR", category: 'vest'},
 ];
 
 function Products() {
