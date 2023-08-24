56a014bd8f9a4dd2bfef5938d3e1374e15fcd266
LucianoEpifani <85083543+lucianoEpifani@users.noreply.github.com>
2023-08-24 19:51:31 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,43 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Nav from './components/Nav/Nav';
 import Search from './components/Search/Search';
-import attitude from './assets/models/models_3.png';
-
-
+import guy from "./assets/models/guy.png";
+import spencer from "./assets/models/spencer.jpg";
+import models3 from "./assets/models/modelos_3.png";
+import pants from "./assets/models/pants.jpg";
 
 function App() {
+  const images = [models3, guy, spencer, pants];
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 5000); 
+
+    return () => clearInterval(interval);
+  }, [images.length]);
+
   return (
     <div className="contenedor">
-       <div className="slider-container">
-            <div className="slider">
-              <img src={attitude} alt="Slider Image" className='girls'/>
-            </div>
+      <header>
+        <Nav />
+      </header>
+      <Search />
+      <div className="slider-container">
+        <div className="slider">
+          {images.map((image, index) => (
+            <div
+              key={index}
+              className={`slider-img ${index === currentImageIndex ? 'visible' : 'hidden'}`}
+              style={{ backgroundImage: `url(${image})` }}
+            />
+          ))}
         </div>
-    <header>
-     <Nav/>
-    </header>
-
-      <Search/>
-  </div>
-  )
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
